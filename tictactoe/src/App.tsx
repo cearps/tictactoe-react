007e3a2787ec2fc50322f6bbcd7101292cd3d132
cear0004 <cear0004@student.monash.edu>
2024-01-31 18:52:30 +1100

    Board working and clicks being handled from board.

---
+++ tictactoe/src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+
+  function handleClick(index: number) {
+    const nextSquares = squares.slice();
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function Square(input: { value: string; onSquareClick: () => void }) {
+  return (
+    <button className="square" onClick={input.onSquareClick}>
+      {input.value}
+    </button>
+  );
+}
+
+export default App;
